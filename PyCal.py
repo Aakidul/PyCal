6c38f19756d67756f705a4c1c9594de563b97659
Aakidul <157734003+Aakidul@users.noreply.github.com>
2024-02-04 17:42:18 +0530

    Add files via upload

---
+++ PyCal.py
@@ -0,0 +1,12 @@
+def main():
+   print ("Welcome to PyCalculator  ^⁠_⁠^")
+   print("\n")
+   while True:
+        user_input = input("ENTER: ")
+        x = user_input.replace("x", "*").replace("÷", "/")
+        result = eval(x)
+        print(result)
+
+
+
+main()
