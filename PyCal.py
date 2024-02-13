149a23e6bfbf9fc6e8c7901ef4b727a6e316276d
Aakidul <157734003+Aakidul@users.noreply.github.com>
2024-02-13 20:34:00 +0530

    Update PyCal.py

--- PyCal.py
+++ PyCal.py
@@ -1,8 +1,8 @@
 def main():
-   print ("Welcome to PyCalculator  ^⁠_⁠^")
+   print ("Welcome to PyCalculator  ^⁠_⁠^  ")
    print("\n")
    while True:
-        user_input = input("ENTER: ")
+        user_input = input("ENTER NUMBER: ")
         x = user_input.replace("x", "*").replace("÷", "/")
         result = eval(x)
         print(f"RESULT: {result}")
